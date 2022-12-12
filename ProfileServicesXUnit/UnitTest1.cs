f8745038bc83619b27734951d0d771c4926a5663
Priyanku <priyankubiswas85@gmail.com>
2022-12-12 02:23:25 +0100

    Static results in Xunit

--- ProfileServicesXUnit/UnitTest1.cs
+++ ProfileServicesXUnit/UnitTest1.cs
@@ -6,6 +6,10 @@ namespace ProfileServicesXUnit
 {
     public class UnitTest1
     {
+        private static String molFileStringBenzene = "\n  Marvin  10310613082D          \n\n  6  6  0  0  0  0            999 V2000\n    0.7145   -0.4125    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    0.0000   -0.8250    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    0.7145    0.4125    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    0.0000    0.8250    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n   -0.7145   -0.4125    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n   -0.7145    0.4125    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n  2  1  2  0  0  0  0\n  3  1  1  0  0  0  0\n  4  3  2  0  0  0  0\n  5  2  1  0  0  0  0\n  6  4  1  0  0  0  0\n  5  6  2  0  0  0  0\nM  END\n";
+        private static int width = 200;
+        private static int height = 200;
+        private static String smilesBenzene = "C1=CC=CC=C1";
         private readonly ITestOutputHelper _testOutputHelper;
         public UnitTest1(ITestOutputHelper testOutputHelper)
         {
@@ -15,7 +19,7 @@ namespace ProfileServicesXUnit
         [Fact]
         public void GetImageFromSmiles_ReturnsPNG()
         {
-            var result = ProfileServicesApi.GetImageFromSmiles("", 200, 200);
+            var result = ProfileServicesApi.GetImageFromSmiles(smilesBenzene, width, height);
 
             //Assert
             _testOutputHelper.WriteLine("Return type " + result.GetType());
@@ -29,7 +33,7 @@ namespace ProfileServicesXUnit
         public void GetSmilesFromMolString_ReturnsSmiles()
         {
             PostRequest postreq = new PostRequest();
-            postreq.molFileString = "\n  Marvin  10310613082D          \n\n  6  6  0  0  0  0            999 V2000\n    0.7145   -0.4125    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    0.0000   -0.8250    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    0.7145    0.4125    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    0.0000    0.8250    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n   -0.7145   -0.4125    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n   -0.7145    0.4125    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n  2  1  2  0  0  0  0\n  3  1  1  0  0  0  0\n  4  3  2  0  0  0  0\n  5  2  1  0  0  0  0\n  6  4  1  0  0  0  0\n  5  6  2  0  0  0  0\nM  END\n";
+            postreq.molFileString = molFileStringBenzene;
             var result = ProfileServicesApi.GetSmilesFromMolString(postreq);
 
             //Assert
@@ -38,7 +42,7 @@ namespace ProfileServicesXUnit
 
             ContentHttpResult content = (ContentHttpResult)result;
             Assert.Equal("text/plain", content.ContentType);
-            Assert.Equal("C1=CC=CC=C1", content.ResponseContent);
+            Assert.Equal(smilesBenzene, content.ResponseContent);
 
         }
     }
