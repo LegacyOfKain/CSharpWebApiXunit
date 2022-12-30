b44cb301929ddff42ca8b92df920cf6b4efb1163
Priyanku <priyankubiswas85@gmail.com>
2022-12-30 01:18:47 +0100

    Updated code to Nova project

--- ProfileServices/MapApiEndpoints.cs
+++ ProfileServices/MapApiEndpoints.cs
@@ -1,4 +1,6 @@
 ﻿using GraphMolWrap;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace ProfileServices
 {
@@ -6,11 +8,67 @@ namespace ProfileServices
     {
         public static IEndpointRouteBuilder MapApiEndpoints(this IEndpointRouteBuilder routes)
         {
+
+            routes.MapPost("/GetImageFromMolFile", GetImageFromMolFile).WithTags("ChemServices");
             routes.MapGet("/GetImageFromSmiles", GetImageFromSmiles).WithTags("ChemServices");
             routes.MapPost("/GetSmilesFromMolString", GetSmilesFromMolString).WithTags("ChemServices");
             routes.MapPost("/GetImageFromMolString", GetImageFromMolString).WithTags("ChemServices");
+            routes.MapGet("/GetMolStringFromSmiles", GetMolStringFromSmiles).WithTags("ChemServices");
+            routes.MapGet("/GetSequenceFromHELM", GetSequenceFromHELM).WithTags("ChemServices")
+                .WithOpenApi(operation => new(operation)
+                {
+                    Summary = "Returns a sequence from HELM String",
+                    Description = "Construct a molecule from a HELM string (currently only supports peptides, RNA and DNA)<br><code>PEPTIDE1{A.C.T.G.C.T.W.G.T.W.E.C.W.C.Q.W}|PEPTIDE2{A.C.T.G.C.T.W.G.T.W.E.Q}$PEPTIDE1,PEPTIDE1,5:R3-14:R3|PEPTIDE2,PEPTIDE1,2:R3-12:R3$$$</code><br><code>RNA1{P.R(A)P.R(U)P}$$$$</code><br><code>RNA1{\\[dR\\](A)P.\\[dR\\](C)P.\\[dR\\](G)P.\\[dR\\](A)}</code>"
+                });
+            routes.MapGet("/GetImageFromHELM", GetImageFromHELM).WithTags("ChemServices")
+                .WithOpenApi(operation => new(operation)
+                {
+                    Summary =
+                    "Returns an image from a HELM string",
+                    Description = "Returns an image from a HELM string (currently only supports peptides, RNA and DNA)<br><code>PEPTIDE1{A.C.T.G.C.T.W.G.T.W.E.C.W.C.Q.W}|PEPTIDE2{A.C.T.G.C.T.W.G.T.W.E.Q}$PEPTIDE1,PEPTIDE1,5:R3-14:R3|PEPTIDE2,PEPTIDE1,2:R3-12:R3$$$</code><br><code>RNA1{P.R(A)P.R(U)P}$$$$</code><br><code>RNA1{\\[dR\\](A)P.\\[dR\\](C)P.\\[dR\\](G)P.\\[dR\\](A)}</code>"
+                });
+            
+            routes.MapGet("/Get3DSDFStringFromSmiles", Get3DSDFStringFromSmiles).WithTags("ChemServices");
 
             return routes;
+        }
+
+        public static IResult GetMolStringFromSmiles(String smiles)
+        {
+            /*
+            if (postRq.molBlock == null || postRq.molBlock.Length == 0)
+            {
+                return BadRequest();
+            }
+            */
+            var mol = RWMol.MolFromSmiles(smiles);
+            return Results.Content(RDKFuncs.MolToMolBlock(mol), "text/plain");
+        }
+
+        public static async Task<IResult> GetImageFromMolFile(IFormFile file, int width, int height)
+        {
+            /*
+            if (file == null || file.Length == 0)
+            {
+                return BadRequest();
+            }
+            */
+
+            var view = new MolDraw2DCairo(width, height);
+            //view.drawOptions().explicitMethyl = true;
+            view.drawOptions().maxFontSize = 10;
+
+            using (var reader = new StreamReader(file.OpenReadStream()))
+            {
+                var molBlock = await reader.ReadToEndAsync();
+                //_logger.LogInformation (molBlock);
+                var toluene = RWMol.MolFromMolBlock(molBlock);
+                //RDKFuncs.prepareMolForDrawing(toluene);
+                view.drawMolecule(toluene);
+                view.finishDrawing();
+            }
+
+            return Results.File(view.getImage().ToArray(), "image/png");
         }
 
         public static IResult GetImageFromSmiles(String smiles, int width, int height)
@@ -20,20 +78,62 @@ namespace ProfileServices
             return Results.File(drawer.getImage().ToArray(), "image/png");
         }
 
-        public static IResult GetSmilesFromMolString(PostRequest request)
+        public static IResult GetSmilesFromMolString(PostRqMolBlockOnly request)
         {
-            Console.WriteLine(request.molFileString);
-            var mol = RWMol.MolFromMolBlock(request.molFileString);           
+            Console.WriteLine(request.molBlock);
+            var mol = RWMol.MolFromMolBlock(request.molBlock);           
             return Results.Content(mol.MolToSmiles(true,true), "text/plain");
         }
 
-        public static IResult GetImageFromMolString(PostRequest2 request)
+        public static IResult GetImageFromMolString(PostRq request)
         {
-            Console.WriteLine(request.molFileString);
-            var mol = RWMol.MolFromMolBlock(request.molFileString);
+            Console.WriteLine(request.molBlock);
+            var mol = RWMol.MolFromMolBlock(request.molBlock);
             var drawer = getDrawer(mol,request.width,request.height);
             return Results.File(drawer.getImage().ToArray(), "image/png");
         }
+
+
+        public static IResult GetSequenceFromHELM(String helmstring)
+        {
+            /*
+            if (postRq.molBlock == null || postRq.molBlock.Length == 0)
+            {
+                return BadRequest();
+            }
+            */
+            var mol = RWMol.MolFromHELM(helmstring, true);
+            return Results.Content(mol.MolToSequence(), "text/plain");
+        }
+
+        public static IResult GetImageFromHELM(String helmstring, int width, int height)
+        {
+            var mol = RWMol.MolFromHELM(helmstring, true);
+            var view = getDrawer(mol, width, height);
+
+            return Results.File(view.getImage().ToArray(), "image/png");
+        }
+
+        //https://stackoverflow.com/questions/70335013/why-i-cant-get-3d-mol-structure-in-rdkit
+        public static IResult Get3DSDFStringFromSmiles(String smiles)
+        {
+
+            var mol = RWMol.MolFromSmiles(smiles);
+            RDKFuncs.addHs(mol);
+            var result = DistanceGeom.EmbedMolecule(mol, 4, 0xf00d);
+            if (result != 0)
+            {
+                //_logger.LogError("result = " + result + " Non zero exit status");
+                return Results.BadRequest("result = " + result + " Non zero exit status");
+            }
+            //Don't delete below 2 lines of code
+            //var c = mol.getConformer();
+            //_logger.LogInformation (c.getPositions().ToString())  ;      
+
+
+            return Results.Content(RDKFuncs.MolToMolBlock(mol), "text/plain");
+        }
+
 
         public static MolDraw2DCairo getDrawer(RWMol mol, int width, int height)
         {
@@ -45,14 +145,14 @@ namespace ProfileServices
         }
     }
 
-    public class PostRequest
+    public class PostRqMolBlockOnly
     {
-        public String molFileString { get; set; }
+        public String molBlock { get; set; }
     }
 
-    public class PostRequest2
+    public class PostRq
     {
-        public String molFileString { get; set; }
+        public string molBlock { get; set; }
         public int width { get; set; }
         public int height { get; set; }
     }
--- ProfileServices/Program.cs
+++ ProfileServices/Program.cs
@@ -1,11 +1,28 @@
+using Microsoft.Extensions.FileProviders;
+using Microsoft.OpenApi.Models;
 using ProfileServices;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    //The generated Swagger JSON file will have these properties.
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Chemservices",
+        Description = "Chemservices API helps you do awesome stuff &#x1F680;" + Environment.MachineName
+        + "&emsp;<a href='/chemservices/GetImageFromSmiles?smiles="
+        //Isomeric SMILES allows for specifying isotopism and stereochemistry of a molecule.
+        // Isomeric SMILES for RNA (poly(U)) , link : https://pubchem.ncbi.nlm.nih.gov/compound/RNA-_poly_U
+        + System.Uri.EscapeDataString("C1=CN(C(=O)NC1=O)[C@H]2[C@@H]([C@@H]([C@H](O2)COP(=O)(O)OC3[C@H](O[C@H]([C@@H]3O)N4C=CC(=O)NC4=O)COP(=O)(O)OC5[C@H](O[C@H]([C@@H]5O)N6C=CC(=O)NC6=O)CO)O)O")
+        + "&width=600&height=400'>Test URL</a> ",
+        Version = "v1"
+    });
+});
 
 var app = builder.Build();
 
@@ -14,8 +31,20 @@ app.UsePathBase("/chemservices");
 app.UseRouting();  // Add explicitly
 
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(c =>
+                // Fix for swagger endpoint under IIS website
+                // https://github.com/domaindrivendev/Swashbuckle.AspNetCore/issues/1245
+                //c.SwaggerEndpoint("/swagger/v1/swagger.json", "Chemservices v1")
+                c.SwaggerEndpoint("v1/swagger.json", "Chemservices v1")
+                );
 
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+           Path.Combine(builder.Environment.ContentRootPath, "StaticFiles")),
+    RequestPath = "/StaticFiles"
+});
 
 app.MapApiEndpoints();
 
